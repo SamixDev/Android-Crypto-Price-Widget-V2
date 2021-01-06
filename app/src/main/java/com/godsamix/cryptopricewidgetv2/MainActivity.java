e40dfe86ddcd07a34e74adf445233b2fd20e54bc
samixdev <samer.hd.ah@gmail.com>
2021-01-06 15:48:39 +0200

    changed template add coin to main view

--- app/src/main/java/com/godsamix/cryptopricewidgetv2/MainActivity.java
+++ app/src/main/java/com/godsamix/cryptopricewidgetv2/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.godsamix.cryptopricewidgetv2.Helpers.SimpleRVAdapter;
 
@@ -20,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new SimpleRVAdapter(new String[] {"1", "2", "3", "4", "5", "6"},new String[] {"11", "22", "33", "44", "55", "66"}));
-        int offsetPx = 10;
+
+        LinearLayout la = findViewById(R.id.linearlay);
+        la.setVisibility(View.GONE);
+    }
+
+    public static void UpdateView(){
+        
     }
 }
