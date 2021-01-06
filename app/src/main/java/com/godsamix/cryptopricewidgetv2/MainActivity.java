dbe71402792c8389b185973e89d26b9ad2f243d7
samixdev <samer.hd.ah@gmail.com>
2021-01-06 15:58:36 +0200

    hide see add coin button

--- app/src/main/java/com/godsamix/cryptopricewidgetv2/MainActivity.java
+++ app/src/main/java/com/godsamix/cryptopricewidgetv2/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -13,21 +14,28 @@ import com.godsamix.cryptopricewidgetv2.Helpers.SimpleRVAdapter;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    public static SimpleRVAdapter adap;
+    public static LinearLayout la;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        la = findViewById(R.id.linearlay);
 
         recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new SimpleRVAdapter(new String[] {"1", "2", "3", "4", "5", "6"},new String[] {"11", "22", "33", "44", "55", "66"}));
+        adap = new SimpleRVAdapter(new String[] {"1", "2", "3", "4", "5", "6"},new String[] {"11", "22", "33", "44", "55", "66"});
+        recyclerView.setAdapter(adap);
 
-        LinearLayout la = findViewById(R.id.linearlay);
-        la.setVisibility(View.GONE);
+        UpdateView();
     }
 
     public static void UpdateView(){
-        
+        if(adap.getItemCount() <= 8){
+            la.setVisibility(View.VISIBLE);
+        }else{
+            la.setVisibility(View.GONE);
+        }
     }
 }
