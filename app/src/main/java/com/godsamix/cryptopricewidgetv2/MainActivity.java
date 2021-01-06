d31e29cd9660d6bc7e30460ea74ffa9370b76475
samixdev <samer.hd.ah@gmail.com>
2021-01-06 16:10:26 +0200

    added footer text

--- app/src/main/java/com/godsamix/cryptopricewidgetv2/MainActivity.java
+++ app/src/main/java/com/godsamix/cryptopricewidgetv2/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.godsamix.cryptopricewidgetv2.Helpers.SimpleRVAdapter;
 
@@ -16,18 +20,32 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     public static SimpleRVAdapter adap;
     public static LinearLayout la;
+    public static TextView footer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        la = findViewById(R.id.linearlay);
 
+        la = findViewById(R.id.linearlay);
         recyclerView = findViewById(R.id.recyclerview);
+        footer = (TextView) findViewById(R.id.footer);
+
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adap = new SimpleRVAdapter(new String[] {"1", "2", "3", "4", "5", "6"},new String[] {"11", "22", "33", "44", "55", "66"});
         recyclerView.setAdapter(adap);
 
+        //footer
+        footer.setText(
+                Html.fromHtml(
+                        "Made by " +
+                                "<a href='https://github.com/SamixDev'>SamixDev</a>" +
+                                " for crypto lovers &#128578;"));
+        footer.setMovementMethod(LinkMovementMethod.getInstance());
+
+        la.setOnClickListener(v -> {
+
+        });
         UpdateView();
     }
 
