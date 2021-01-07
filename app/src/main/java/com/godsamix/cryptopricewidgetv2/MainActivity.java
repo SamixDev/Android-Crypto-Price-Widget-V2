66cdf31392de7dd35ea82ef000b0f38741248f8c
samixdev <samer.hd.ah@gmail.com>
2021-01-07 17:54:27 +0200

    gson.FromJson test works

--- app/src/main/java/com/godsamix/cryptopricewidgetv2/MainActivity.java
+++ app/src/main/java/com/godsamix/cryptopricewidgetv2/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -15,17 +16,28 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.godsamix.cryptopricewidgetv2.Controllers.CoinsListController;
 import com.godsamix.cryptopricewidgetv2.Helpers.SimpleRVAdapter;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     public static SimpleRVAdapter adap;
+    @SuppressLint("StaticFieldLeak")
     public static LinearLayout la;
+    @SuppressLint("StaticFieldLeak")
     public static TextView footer;
     public  static SharedPreferences sharedPreferences;
     public static String coinsJsonList;
-
+    public static String coinsarray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +54,16 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adap = new SimpleRVAdapter(new String[] {"1", "2", "3", "4", "5", "6"},new String[] {"11", "22", "33", "44", "55", "66"});
         recyclerView.setAdapter(adap);
+
+        //Grab saved coinsList from shared prefs
+        coinsarray = "[{\"id\":\"01coin\",\"symbol\":\"zoc\",\"name\":\"01coin\"}," +
+                "{\"id\":\"aelf\",\"symbol\":\"elf\",\"name\":\"elf\"}]";
+        Gson gson = new Gson();
+      //  CoinsListController con = gson.fromJson(coinsarray, CoinsListController.class);
+       List<CoinsListController> jj = gson.fromJson(coinsarray, new TypeToken<List<JsonObject>>(){}.getType());
+        Log.e("tst" , jj.toString());
+
+
 
         //footer
         footer.setText(
