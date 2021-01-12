d7eb5734321a299b3cce0481990cb76f13637572
samixdev <samer.hd.ah@gmail.com>
2021-01-12 15:28:15 +0200

    created widget layout recycler with coins list

--- app/src/main/java/com/godsamix/cryptopricewidgetv2/CryptoPrice.java
+++ app/src/main/java/com/godsamix/cryptopricewidgetv2/CryptoPrice.java
@@ -3,13 +3,16 @@ package com.godsamix.cryptopricewidgetv2;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
+
+import static com.godsamix.cryptopricewidgetv2.MainActivity.sharedPreferences;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class CryptoPrice extends AppWidgetProvider {
-
+    public static String coinsJsonList;
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
@@ -17,6 +20,18 @@ public class CryptoPrice extends AppWidgetProvider {
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.crypto_price);
         views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        coinsJsonList = sharedPreferences.getString("coins", "");
+        if (coinsJsonList.isEmpty()){
+            String btcprefs = "[{\"id\":\"bitcoin\",\"symbol\":\"btc\",\"name\":\"Bitcoin\",\"img\":\"\"}]";
+            SharedPreferences.Editor myEdit = sharedPreferences.edit();
+            myEdit.putString("coins",btcprefs);
+            myEdit.apply();
+        }
+
+
+
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
